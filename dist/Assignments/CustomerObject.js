20d15c51ca8f482f35a0efc7ea84ae1dad5e3a7f
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-14 12:33:29 +0530

    question4

---
+++ dist/Assignments/CustomerObject.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const customers = [
+    {
+        id: 1, name: "krishna", email: "krishna@gmail.com", phone: 7306410867
+    },
+    {
+        id: 2, name: "Megha", email: "megha@gmail.com"
+    },
+    {
+        id: 3, name: "Gowri", email: "gowri@gmail.com", phone: 9037534932
+    }
+];
+for (let customer of customers) {
+    console.log("Customer Id:", customer.id);
+    console.log("Name:", customer.name);
+    console.log("Email:", customer.email);
+    console.log("Phone:", customer.phone);
+}
+//# sourceMappingURL=CustomerObject.js.map
