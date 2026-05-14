76e271221697f50f06e1d703ee55f92607b2bd53
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-14 11:57:46 +0530

    question3

---
+++ dist/Assignments/NestedUserObject.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = {
+    name: "Krishna",
+    age: 23,
+    address: {
+        HouseAddress: "Vilathara mele veedu",
+        city: "Trivandrum",
+        pincode: 695527
+    }
+};
+console.log("User Name:", user.name);
+console.log("City :", user.address.city);
+console.log("Pincode:", user.address.pincode);
+//# sourceMappingURL=NestedUserObject.js.map
---
+++ src/Assignments/NestedUserObject.ts
@@ -0,0 +1,15 @@
+const user={
+    name:"Krishna",
+    age:23,
+
+    address:{
+        HouseAddress:"Vilathara mele veedu",
+        city:"Trivandrum",
+        pincode:695527
+
+    }
+}
+console.log("User Name:",user.name)
+console.log("City :",user.address.city)
+console.log("Pincode:",user.address.pincode)
+export {};
