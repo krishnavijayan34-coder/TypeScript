d2cb0d7c7ae9e9ae559fe36e68c75b312d0c523e
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-15 17:41:20 +0530

    Generics question2

---
+++ dist/Assignments/GenericInterface.js
@@ -0,0 +1,10 @@
+"use strict";
+const multiply = (a, b) => {
+    return a * b;
+};
+console.log("Multiplication Result:", multiply(8, 4));
+const joinText = (a, b) => {
+    return a + b;
+};
+console.log("joined Text:", joinText("Welcome", " Krishna"));
+//# sourceMappingURL=GenericInterface.js.map
---
+++ src/Assignments/GenericInterface.ts
@@ -0,0 +1,11 @@
+interface Operation<T>{
+    (a:T,b:T):T;
+}
+const multiply:Operation<number>=(a,b)=>{
+    return a*b;
+};
+console.log("Multiplication Result:",multiply(8,4));
+const joinText:Operation<string>=(a,b)=>{
+    return a+b;
+};
+console.log("joined Text:",joinText("Welcome"," Krishna"));
