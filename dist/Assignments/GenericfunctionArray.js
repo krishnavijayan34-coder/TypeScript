1d460cbb5eec45e6aff9d440a82ffb4a4a7e3192
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-15 18:05:44 +0530

    Generics question 2

--- dist/Assignments/GenericfunctionArray.js
+++ dist/Assignments/GenericfunctionArray.js
@@ -1,8 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function printArray(arr) {
     console.log(arr);
 }
 printArray([1, 2, 3]);
 printArray(["Rose", "Jasmine", "Lilly"]);
-printArray([true, false, true]);
+printArray([true, false]);
 //# sourceMappingURL=GenericfunctionArray.js.map
--- src/Assignments/GenericfunctionArray.ts
+++ src/Assignments/GenericfunctionArray.ts
@@ -1,6 +1,11 @@
 function printArray<T>(arr:T[]):void{
     console.log(arr);
 }
+
 printArray<number>([1,2,3]);
+
 printArray<string>(["Rose","Jasmine","Lilly"]);
-printArray<boolean>([true,false,true]);
+
+printArray<boolean>([true,false]);
+
+export {};
