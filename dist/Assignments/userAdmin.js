81dbaad3bcb41e520cbbe111688152a0a1044bbc
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-15 13:33:03 +0530

    password added

--- dist/Assignments/userAdmin.js
+++ dist/Assignments/userAdmin.js
@@ -3,19 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class User {
     name;
     email;
-    constructor(name, email) {
+    password;
+    constructor(name, email, password) {
         this.name = name;
         this.email = email;
+        this.password = password;
     }
     login(username) {
         console.log(username + " is logged in");
+    }
+    forgotPassword() {
+        console.log("password reset link sent to " + this.email);
     }
 }
 class Admin extends User {
     role;
     users = [];
-    constructor(name, email, role) {
-        super(name, email);
+    constructor(name, email, password, role) {
+        super(name, email, password);
         this.role = role;
     }
     login(username) {
@@ -25,10 +30,15 @@ class Admin extends User {
     viewUsers() {
         console.log(this.users);
     }
+    adminDetails() {
+        console.log(this.name);
+        console.log(this.email);
+    }
 }
-let user1 = new User("Krishna", "krishnavijayan@gmail.com");
+let user1 = new User("Krishna", "krishnavijayan@gmail.com", "Krishna@123");
 user1.login("Krishna");
-let admin1 = new Admin("Admin", "admin@gmail.com", "Manager");
+user1.forgotPassword();
+let admin1 = new Admin("Admin", "admin@gmail.com", "admin123", "Manager");
 admin1.login("Krishna");
 admin1.login("Arjun");
 admin1.viewUsers();
--- src/Assignments/userAdmin.ts
+++ src/Assignments/userAdmin.ts
@@ -1,22 +1,27 @@
 
 class User{
-    name:string;
-    email:string;
-    constructor(name:string,email:string){
+   public name:string;
+   protected email:string;
+   private password:string;
+    constructor(name:string,email:string,password:string){
         this.name=name;
         this.email=email;
+        this.password=password;
     }
     login(username:string){
     console.log(username + " is logged in");
     
+    }
+    forgotPassword(){
+        console.log("password reset link sent to " + this.email);
     }
 }
 
 class Admin extends User{
     role:string;
     users:string[]=[];
-    constructor(name:string,email:string,role:string){
-        super(name,email);
+    constructor(name:string,email:string,password:string,role:string){
+        super(name,email,password);
         this.role=role;
     }
     login(username: string){
@@ -26,10 +31,16 @@ class Admin extends User{
     viewUsers(){
         console.log(this.users);
     }
+    adminDetails(){
+        console.log(this.name);
+        console.log(this.email);
+    }
 }
-let user1=new User("Krishna","krishnavijayan@gmail.com");
+let user1=new User("Krishna","krishnavijayan@gmail.com","Krishna@123");
 user1.login("Krishna");
-let admin1=new Admin("Admin","admin@gmail.com","Manager")
+
+user1.forgotPassword();
+let admin1=new Admin("Admin","admin@gmail.com","admin123","Manager")
 admin1.login("Krishna");
 admin1.login("Arjun");
 admin1.viewUsers();
