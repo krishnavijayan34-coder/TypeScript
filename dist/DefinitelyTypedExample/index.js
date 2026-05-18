be012a996c58a8553bf5b4e1a2d553295f181af6
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-18 18:26:31 +0530

    modified

---
+++ dist/DefinitelyTypedExample/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = __importDefault(require("lodash"));
+const numbers = [1, 2, 3, 4];
+const reversed = lodash_1.default.reverse(numbers);
+console.log(reversed);
+//# sourceMappingURL=index.js.map
---
+++ src/DefinitelyTypedExample/index.ts
@@ -0,0 +1,7 @@
+import _ from "lodash";
+
+const numbers = [1, 2, 3, 4];
+
+const reversed = _.reverse(numbers);
+
+console.log(reversed);
