5dc3c0e0e7ebecb4070209c558a2ab9eb959f207
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-19 12:50:35 +0530

    modified

--- src/api-routes-types/routes.ts
+++ src/api-routes-types/routes.ts
@@ -2,8 +2,8 @@ export type HttpMethod="GET"|"POST"|"PUT"|"DELETE";
 
 export type Resource="users"|"products"|"orders";
 
-type BaseRoue<T extends HttpMethod,K extends Resource>=`${T}/${K}`;
+type BaseRoute<T extends HttpMethod,K extends Resource>=`${T}/${K}`;
 
 type IdRoute<T extends HttpMethod,K extends Resource>=`${T}/${K}/:id`;
 
-export type ApiRoute=|BaseRoue<HttpMethod,Resource>|IdRoute<HttpMethod,Resource>;
+export type ApiRoute=|BaseRoute<HttpMethod,Resource>|IdRoute<HttpMethod,Resource>;
